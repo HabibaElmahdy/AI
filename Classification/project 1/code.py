8c7eb15ee213e518d99f06031bba35e3ce79356f
Habiba Elmahdy <68806897+HabibaElmahdy@users.noreply.github.com>
2023-09-17 14:07:15 +0300

    Update code.py

--- Classification/project 1/code.py
+++ Classification/project 1/code.py
@@ -1,38 +1,34 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Sep 17 13:31:11 2023
+Created on Sun Sep 17 12:05:22 2023
 
 @author: Habiba ELmahdy
 """
-
-import pandas as pd
+#import pandas as pd
+import numpy as np
+#import matplotlib as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.multiclass import OneVsRestClassifier #OR OVO
 
-        #read
-data = pd.read_csv('iris.csv')
+data = pd.read_csv('heart_failure_clinical_records_dataset.csv')
 
-        #split i/o
-# if str in y dont change
-
+        #split data i/o > x/y
+        
 x = data.iloc[:,:-1]
 y = data.iloc[:,-1]
-x_tr, x_ts, y_tr, y_ts = train_test_split(x, y, train_size=0.90)
 
-        #ova1
+        #if there null
+#print(data.isnull().sum())
+
+        #split train & test data
+
+x_train, x_test, y_train, y_test = train_test_split(x,y, train_size = 0.95)
+
+        #train
         
-m = LogisticRegression(multi_class='ovr')
-m.fit(x_tr,y_tr)
+m = LogisticRegression()
+m.fit(x_train, y_train)
 
-print(m.score(x_tr, y_tr))
-print(m.score(x_ts, y_ts))
+print(m.score(x_train, y_train))
 
-        #ova2
-
-m = OneVsRestClassifier(LogisticRegression()) #OR OVO
-m.fit(x_tr, y_tr)
-
-print(m.score(x_tr, y_tr))
-print(m.score(x_ts, y_ts))
-
+print(m.score(x_test, y_test))
